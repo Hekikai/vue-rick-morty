bee121a883a0c8297531629128ad50d4fc6f2882
Artem Kolesnikov <akolesnikov@stimul.app>
2023-02-02 18:03:02 +0300

    adding dist

--- src/helpers/index.ts
+++
@@ -1,8 +0,0 @@
-export const reformatDate = (date: string) => {
-    return new Date(date).toLocaleDateString('en-En', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    })
-}
--- src/service/api.ts
+++
@@ -1,5 +0,0 @@
-class BaseService {
-    BASE_PATH = 'https://rickandmortyapi.com/api/'
-}
-
-export default BaseService
